ec3443c20455f36fe2d3e000e644e33bad8f3c93
zhyea <zhangrui@zhyea.com>
2023-12-20 22:43:00 +0800

    1.自启动配置
    Change-Id: Ia9f66520cefa8036966d42e1ded577def13226b0

--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
+++ mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
@@ -3,16 +3,27 @@ package org.chobit.mocko;
 
 import org.chobit.mocko.annotations.Mocko;
 import org.chobit.mocko.client.MockoProperties;
+import org.chobit.mocko.simple.MockoInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 自启动配置
+ *
  * @author rui.zhang
  */
 @Configuration
 @ConditionalOnClass({Mocko.class})
 @EnableConfigurationProperties(MockoProperties.class)
 public class MockoAutoConfiguration {
+
+
+    @Bean
+    public MockoInterceptor mockoInterceptor() {
+        return new MockoInterceptor();
+    }
+
 
 }
