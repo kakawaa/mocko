f7ccfb7278676c8fff17c63e5108f4a1acf18733
zhyea <zhangrui@zhyea.com>
2023-12-22 22:30:28 +0800

    1.自启动配置-2
    Change-Id: Ib5ca006d4fe4f6b53273e590392beb9f73a7d097

--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
+++ mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
@@ -4,10 +4,14 @@ package org.chobit.mocko;
 import org.chobit.mocko.annotations.Mocko;
 import org.chobit.mocko.client.MockoProperties;
 import org.chobit.mocko.simple.MockoInterceptor;
+import org.chobit.mocko.simple.MockoPointcutSourceAdvisor;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
 
 /**
  * 自启动配置
@@ -21,8 +25,19 @@ public class MockoAutoConfiguration {
 
 
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public MockoInterceptor mockoInterceptor() {
         return new MockoInterceptor();
+    }
+
+
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public MockoPointcutSourceAdvisor quickLogSource(MockoInterceptor quickLogInterceptor) {
+        MockoPointcutSourceAdvisor advisor = new MockoPointcutSourceAdvisor();
+        advisor.setAdvice(quickLogInterceptor);
+        advisor.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        return advisor;
     }
 
 
--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/simple/MockoAspectSupport.java
+++ mocko-spring-boot-starter/src/main/java/org/chobit/mocko/simple/MockoAspectSupport.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Method;
  *
  * @author rui.zhang
  */
-public abstract class MockoAspectSupport {
+public class MockoAspectSupport {
 
 
 }
