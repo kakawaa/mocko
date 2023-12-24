f80e57345a94f0dff9452e7bf311a3677f5bfaaf
zhyea <zhangrui@zhyea.com>
2023-12-24 23:04:36 +0800

    1. 调整依赖
    Change-Id: Idad994514b20b81a9a9b2da2d9a9b4ce044c42ad

--- mocko-server/src/main/java/org/chobit/mocko/MockoApplication.java
+++ mocko-server/src/main/java/org/chobit/mocko/MockoApplication.java
@@ -3,7 +3,6 @@ package org.chobit.mocko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author rui.zhang
  */
 @EnableCaching
-@EnableScheduling
 @SpringBootApplication
 public class MockoApplication {
 
