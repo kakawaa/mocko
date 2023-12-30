f9b80bf9ae6103aa3ccba22ac4b726389e5cc927
zhyea <zhangrui@zhyea.com>
2023-12-30 23:01:00 +0800

    1. 基础数据维护-2

--- doc/table.sql
+++ doc/table.sql
@@ -2,8 +2,29 @@
 -- mocko数据库管理
 --
 
+--
 -- 应用信息表
+--
+create table mocko.m_user
+(
+    id          int unsigned not null default 0 auto_increment comment 'id',
 
+    nick_name   varchar(32)  not null default '' comment '昵称',
+    username    varchar(32)  not null default '' comment '用户名',
+    password    varchar(32)  not null default '' comment '密码',
+
+    operator_id int          not null default 0 comment '操作人ID',
+    deleted     int          not null default 0 comment '删除标记',
+    create_time datetime     not null default now() comment '创建时间',
+    update_time timestamp    not null default current_timestamp on update current_timestamp comment '更新时间',
+    primary key (id)
+) engine = myisam
+  character set = utf8mb4
+  collate = utf8mb4_bin comment = '退款信息总览表';
+
+--
+-- 应用信息表
+--
 create table mocko.m_app
 (
     id          int unsigned not null default 0 auto_increment comment 'id',
@@ -11,7 +32,8 @@ create table mocko.m_app
     app_id      varchar(32)  not null default '' comment '应用ID',
     app_name    varchar(32)  not null default '' comment '应用名称',
 
-    deleted     int                   default 0,
+    operator_id int          not null default 0 comment '操作人ID',
+    deleted     int          not null default 0 comment '删除标记',
     create_time datetime     not null default now() comment '创建时间',
     update_time timestamp    not null default current_timestamp on update current_timestamp comment '更新时间',
     primary key (id)
---
+++ mocko-server/src/main/java/org/chobit/mocko/model/entity/User.java
@@ -0,0 +1,26 @@
+package org.chobit.mocko.model.entity;
+
+/**
+ * 用户信息
+ *
+ * @author rui.zhang
+ */
+public class User extends BaseEntity {
+
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+}
