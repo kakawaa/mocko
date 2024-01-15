ef466be2b834939ac1b79272e0d4121bb5768164
zhyea <zhangrui@zhyea.com>
2024-01-15 23:42:21 +0800

    1. 添加Swagger支持
    Change-Id: I4815abb45583200fbecea63eeff1f092782e83da

--- mocko-server/src/main/java/org/chobit/mocko/controller/AppController.java
+++ mocko-server/src/main/java/org/chobit/mocko/controller/AppController.java
@@ -1,7 +1,10 @@
 package org.chobit.mocko.controller;
 
+import org.chobit.mocko.model.request.AppModifyRequest;
 import org.chobit.mocko.spring.ResponseWrapper;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +20,7 @@ public class AppController {
 
 
     @PostMapping("/update")
-    public Boolean update() {
+    public Boolean update(@Validated @RequestBody AppModifyRequest request) {
         return false;
     }
 
--- mocko-server/src/main/java/org/chobit/mocko/model/request/AppModifyRequest.java
+++ mocko-server/src/main/java/org/chobit/mocko/model/request/AppModifyRequest.java
@@ -2,18 +2,28 @@ package org.chobit.mocko.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * 应用信息更新请求
+ *
  * @author rui.zhang
  */
 @Data
 public class AppModifyRequest {
 
 
-
+    /**
+     * 应用ID
+     */
+    @NotBlank(message = "应用Id不可为空")
     private String appId;
 
 
+    /**
+     * 应用名称
+     */
+    @NotBlank(message = "应用名称不可为空")
     private String appName;
 
 }
