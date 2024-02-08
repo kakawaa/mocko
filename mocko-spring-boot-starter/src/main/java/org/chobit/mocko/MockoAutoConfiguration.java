55529533a94c3fa2fbb363f1a18db42812477f84
zhyea <zhangrui@zhyea.com>
2024-02-08 23:19:59 +0800

    1. 分析eager init问题-2

--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
+++ mocko-spring-boot-starter/src/main/java/org/chobit/mocko/MockoAutoConfiguration.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Role;
+import org.springframework.context.annotation.*;
 import org.springframework.core.Ordered;
 
 /**
@@ -43,6 +40,7 @@ public class MockoAutoConfiguration {
 
 
     @Bean
+    @Scope("prototype")
     @ConditionalOnMissingBean
     public Mocko.Builder mockoBuilder() {
         return Mocko.builder();
