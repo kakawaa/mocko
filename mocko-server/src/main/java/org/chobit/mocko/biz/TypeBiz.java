22a5dcc2b219943b84a72906bad76cc6741c1aa6
zhyea <zhangrui@zhyea.com>
2024-03-25 23:06:08 +0800

    1. 开始构建类信息树-3

--- mocko-server/src/main/java/org/chobit/mocko/biz/TypeBiz.java
+++ mocko-server/src/main/java/org/chobit/mocko/biz/TypeBiz.java
@@ -2,7 +2,6 @@ package org.chobit.mocko.biz;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.chobit.commons.model.Reference;
 import org.chobit.commons.model.TreeNode;
 import org.chobit.commons.model.Tuple2;
 import org.chobit.commons.utils.Collections2;
@@ -13,10 +12,10 @@ import org.chobit.mocko.service.TypeService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.chobit.commons.constans.Symbol.EMPTY;
@@ -73,11 +72,33 @@ public class TypeBiz {
     }
 
 
-    private List<String> findParentPkg(List<Tuple2<String, String>> classList, final String parent) {
+    private void buildClassTree(List<Tuple2<String, String>> classList, TreeNode<String> parentNode) {
+        String parent = this.buildParentPackage(parentNode);
 
-        List<String> result = new LinkedList<>();
+        Set<String> subSet = this.findParentPkg(classList, parent);
+        while (subSet.size() == 1) {
+            String sub = subSet.iterator().next();
+            parent = parent + POINT + sub;
 
-        String line = parent;
+            parentNode.setValue(parentNode.getValue() + POINT + sub);
+
+            subSet = this.findParentPkg(classList, parent);
+        }
+
+        if (Collections2.isEmpty(subSet)) {
+            return;
+        }
+
+        for (String sub : subSet) {
+
+        }
+
+    }
+
+
+    private Set<String> findParentPkg(List<Tuple2<String, String>> classList, final String parent) {
+
+        Set<String> result = new HashSet<>(2);
 
         Iterator<Tuple2<String, String>> itr = classList.iterator();
         while (itr.hasNext()) {
@@ -94,35 +115,53 @@ public class TypeBiz {
                 continue;
             }
 
+            // 处理包名
             String tmp = pkg;
-            if (isNotBlank(parent)) {
-                tmp = pkg.replace(parent, EMPTY);
+            if (isNotBlank(parent) || ObjKit.nonEquals(POINT, parent)) {
+                // 去掉parent相关的部分（parent为空时是第一次匹配）
+                tmp = pkg.substring(parent.length());
             }
 
-            if (isBlank(tmp) || Objects.equals(tmp, POINT)) {
+            if (isBlank(tmp)) {
+                // 处理后子包为空，只剩下类，新增子节点
                 result.add(t._2);
                 itr.remove();
                 continue;
             }
 
-            tmp = tmp.substring(1);
-
+            // 提取子目录名称
             String subPkg = tmp;
             int idx = tmp.indexOf(POINT);
             if (idx > 0) {
                 subPkg = tmp.substring(0, idx);
             }
 
-            if(result.isEmpty() || result.contains(line)){
-                line = line + POINT + subPkg;
-            }else{
-                result.add(subPkg);
-            }
-
-
+            result.add(subPkg);
         }
 
         return result;
+    }
+
+
+    /**
+     * 构建完整的上级包名
+     *
+     * @param node 当前节点
+     * @return 完整的上级包名
+     */
+    private String buildParentPackage(TreeNode<String> node) {
+
+        StringBuilder builder = new StringBuilder();
+
+        TreeNode<String> tmp = node;
+        while (null != tmp) {
+            if (isNotBlank(tmp.getValue())) {
+                builder.insert(0, tmp.getValue() + POINT);
+            }
+            tmp = tmp.getParent();
+        }
+
+        return builder.toString();
     }
 
 
@@ -141,41 +180,5 @@ public class TypeBiz {
         String clazz = classFullName.substring(idx);
         return new Tuple2<>(pkg, clazz);
     }
-
-
-    /**
-     * 构建类树
-     *
-     * @param typeList 类信息集合
-     */
-    private void buildTypeTree(List<Reference<String>> typeList, TreeNode<String> parent) {
-        Iterator<Reference<String>> itr = typeList.iterator();
-        String tmp = null;
-        while (itr.hasNext()) {
-            Reference<String> ref = itr.next();
-            if (isBlank(ref.get())) {
-                itr.remove();
-                continue;
-            }
-
-            int idx = ref.get().indexOf(POINT);
-
-            if (idx < 0) {
-                // 此处剩下的只有类名了
-                parent.addChild(ref.get());
-                itr.remove();
-                continue;
-            }
-
-            String pkg = ref.get().substring(0, idx);
-            if (isBlank(tmp) || ObjKit.nonEquals(pkg, tmp)) {
-                ref.set(ref.get().substring(idx));
-            } else {
-
-            }
-            tmp = pkg;
-        }
-    }
-
 
 }
