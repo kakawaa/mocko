7e8f85b9e91186e8cd12ba05856490a953998d83
zhyea <zhangrui@zhyea.com>
2024-03-30 23:32:48 +0800

    1. 添加获取类信息接口

--- mocko-server/src/main/java/org/chobit/mocko/server/controller/TypeController.java
+++ mocko-server/src/main/java/org/chobit/mocko/server/controller/TypeController.java
@@ -1,8 +1,16 @@
 package org.chobit.mocko.server.controller;
 
+import org.chobit.commons.model.TreeNode;
+import org.chobit.mocko.server.biz.TypeBiz;
 import org.chobit.mocko.server.config.response.ResponseWrapper;
+import org.chobit.mocko.server.model.request.AppIdRequest;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 类相关接口
@@ -11,8 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @ResponseWrapper
 @RestController
-@RequestMapping("/admin/type")
+@RequestMapping("/admin/classes")
 public class TypeController {
 
 
+    @Resource
+    private TypeBiz typeBiz;
+
+
+    @PostMapping("/all")
+    public TreeNode<String> findClasses(@Validated @RequestBody AppIdRequest request) {
+        return typeBiz.obtainTree(request.getAppId());
+    }
 }
