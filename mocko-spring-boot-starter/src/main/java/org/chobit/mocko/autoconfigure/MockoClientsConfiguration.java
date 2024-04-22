477580e384e6c32f9978bf0ae3251688a1ab9200
zhyea <zhangrui@zhyea.com>
2024-04-22 19:59:42 +0800

    1. 优化请求解析能力

--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/autoconfigure/MockoClientsConfiguration.java
+++ mocko-spring-boot-starter/src/main/java/org/chobit/mocko/autoconfigure/MockoClientsConfiguration.java
@@ -1,11 +1,16 @@
 package org.chobit.mocko.autoconfigure;
 
+import org.chobit.mocko.autoconfigure.support.SpringDecoder;
+import org.chobit.mocko.core.Decoder;
 import org.chobit.mocko.core.MockoProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +27,20 @@ public class MockoClientsConfiguration {
 
 
     private static final Logger logger = LoggerFactory.getLogger(MockoClientsConfiguration.class);
+
+
+
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Decoder mockoDecoder(){
+        return new SpringDecoder(messageConverters);
+    }
+
 
 
     @Bean
