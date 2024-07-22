db8916aca7ee3bbc05e42da28e0f9f6c2b041b24
zhyea <zhangrui@zhyea.com>
2024-07-22 23:14:38 +0800

    1. 启动去mp

--- mocko-server/src/main/java/org/chobit/mocko/server/service/mapper/MethodMapper.java
+++ mocko-server/src/main/java/org/chobit/mocko/server/service/mapper/MethodMapper.java
@@ -2,6 +2,8 @@ package org.chobit.mocko.server.service.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.chobit.mocko.server.model.entity.MethodEntity;
 
 /**
@@ -11,4 +13,16 @@ import org.chobit.mocko.server.model.entity.MethodEntity;
  */
 @Mapper
 public interface MethodMapper extends BaseMapper<MethodEntity> {
+
+
+	/**
+	 * 根据methodId获取方法信息
+	 *
+	 * @param methodId 方法ID
+	 * @return 方法信息
+	 */
+	@Select({"select * from m_method where method_id=#{methodId} limit 1"})
+	MethodEntity getByMethodId(@Param("methodId") String methodId);
+
+
 }
