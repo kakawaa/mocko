6b91039417a6e2084863a4e7f2b95b6592f76dae
zhyea <zhangrui@zhyea.com>
2024-07-23 08:56:33 +0800

    1. 启动去MP

--- mocko-server/src/main/java/org/chobit/mocko/server/service/mapper/AppMapper.java
+++ mocko-server/src/main/java/org/chobit/mocko/server/service/mapper/AppMapper.java
@@ -2,8 +2,21 @@ package org.chobit.mocko.server.service.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.chobit.mocko.server.model.entity.AppEntity;
 
 @Mapper
-public interface AppMapper  extends BaseMapper<AppEntity> {
+public interface AppMapper extends BaseMapper<AppEntity> {
+
+
+	@Select({"select * from m_app where app_id=#{appId} limit 1"})
+	AppEntity getByAppId(@Param("appId") String appId);
+
+
+	@Update({"update m_app set app_name=#{appName} where app_id=#{appId} "})
+	boolean modifyAppName(@Param("appName") String appName, @Param("appId")String appId);
+
+
 }
