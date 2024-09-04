f0f2936327ed3025883c9e5f5323ce08bf128672
zhyea <zhangrui@zhyea.com>
2024-09-04 11:06:20 +0800

    1. 调整SessionInterceptor

--- mocko-server/src/main/java/org/chobit/mocko/server/config/intercept/SessionInterceptor.java
+++ mocko-server/src/main/java/org/chobit/mocko/server/config/intercept/SessionInterceptor.java
@@ -15,30 +15,24 @@ import javax.servlet.http.HttpServletResponse;
 public class SessionInterceptor implements HandlerInterceptor {
 
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		String sessionId = request.getSession(true).getId();
-		System.out.println(Thread.currentThread().getId() + " ------------------BEGIN " + sessionId);
-		AuthContext.setSessionId(request.getRequestedSessionId());
-
-		return true;
-	}
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String sessionId = request.getSession(true).getId();
+        AuthContext.setSessionId(sessionId);
+        return true;
+    }
 
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
-			throws Exception {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) {
 
-		System.out.println(Thread.currentThread().getId() + " ------------------TEM  " + request.getRequestedSessionId());
-	}
+    }
 
 
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-
-		System.out.println(Thread.currentThread().getId() + " ------------------END  " + request.getRequestedSessionId());
-		AuthContext.removeSessionId();
-	}
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception ex) {
+        AuthContext.removeSessionId();
+    }
 }
