b332a062e0ff2c1e813a46ceeac47012b1833ae4
zhyea <zhangrui@zhyea.com>
2024-09-22 11:57:02 +0800

    1. 合并前后端代码到一个仓库中，前端代码放在前端目录，后端代码放在后端目录。

--- mocko-server/src/main/java/org/chobit/mocko/server/model/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.chobit.mocko.server.model;
--- mocko-spring-boot-starter/src/main/java/org/chobit/mocko/autoconfigure/support/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package org.chobit.mocko.autoconfigure.support;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
